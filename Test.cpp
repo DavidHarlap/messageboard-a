8d66ffff710bc5b9b4b9b0a4a71f1b328cc6a5ce
DavidHarlap <73073720+DavidHarlap@users.noreply.github.com>
2021-03-23 11:22:01 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -5,12 +5,10 @@
 
 TEST_CASE("empty board"){
     ariel::Board b1;
-    //CHECK(b1.get_rownum==0);
-    //CHECK(b1.get_colnum==0);
     CHECK_THROWS(b1.read(0,0,ariel::Direction::Horizontal,1));
 }
 
-TEST_CASE("bad - check of read") {
+TEST_CASE("bad check of read") {
     ariel::Board b1;
     b1.post(0,0,ariel::Direction::Horizontal,"hi my name");
     b1.post(1,3,ariel::Direction::Horizontal,"is");
@@ -24,18 +22,16 @@ TEST_CASE("bad - check of read") {
         ______I___
         ______D___
     */
-    //CHECK(b1.get_rownum==6);
-    //CHECK(b1.get_colnum==10);
     CHECK_THROWS(b1.read(0,0,ariel::Direction::Horizontal,0)); //length can not be 0.
     CHECK_THROWS(b1.read(8,0,ariel::Direction::Horizontal,3)); // row number out of range.
     CHECK_THROWS(b1.read(1,12,ariel::Direction::Vertical,3)); //col number out of range.
     CHECK_THROWS(b1.read(0,4,ariel::Direction::Horizontal,12)); // Horizontal length is not mach to the real length.
-    CHECK_THROWS(b1.read(0,4,ariel::Direction::Vertical,9); //Vertical length is match to the real length.
-    CHECK_THROWS(b1.read(6,4,ariel::Direction::Vertical,3); //Vertical length is geting out of range.
-    CHECK_THROWS(b1.read(6,4,ariel::Direction::Horizontal,7); //Horizontal length is geting out of range.
+    CHECK_THROWS(b1.read(0,4,ariel::Direction::Vertical,9)); //Vertical length is match to the real length.
+    CHECK_THROWS(b1.read(6,4,ariel::Direction::Vertical,3)); //Vertical length is geting out of range.
+    CHECK_THROWS(b1.read(6,4,ariel::Direction::Horizontal,7)); //Horizontal length is geting out of range.
 }
 
-TEST_CASE("check if the board grow"){
+TEST_CASE("check if all the board grow"){
     ariel::Board b1;
     b1.post(0,0,ariel::Direction::Horizontal,"Hi");
     b1.post(8,15,ariel::Direction::Horizontal,"is");
@@ -44,7 +40,7 @@ TEST_CASE("check if the board grow"){
     CHECK(b1.read(0,47,ariel::Direction::Vertical,5)=="_____");
 }
 
-TEST_CASE("ost one on 2"){
+TEST_CASE("post one on 2"){
     ariel::Board b1;
     b1.post(3,3,ariel::Direction::Horizontal,"BALAGAN");
     CHECK(b1.read(3,3,ariel::Direction::Horizontal,7)=="BALAGAN");
@@ -54,23 +50,30 @@ TEST_CASE("ost one on 2"){
     CHECK(b1.read(3,3,ariel::Direction::Horizontal,7)=="BxLAbAN");
     b1.post(3,6,ariel::Direction::Horizontal,"hi!");
     CHECK(b1.read(3,3,ariel::Direction::Horizontal,7)=="BxLhi!N");
+    CHECK(b1.read(4,4,ariel::Direction::Horizontal,4)== "y__c");
+    b1.post(4,0,ariel::Direction::Horizontal,"this is very very very long string");
+    CHECK(b1.read(4,4,ariel::Direction::Horizontal,4)== " is ");
+
+
 }
 
 TEST_CASE("random post"){
     string str[]={"this","is","random","board"};
     ariel::Board b1;
-    unsigned int r= rand() % 150;
-    unsigned int c= rand() % 150;
-    int strIndex = rand() % 4;
     
-    for (size_t i = 0; i < 45; i++) {
+    for (size_t i = 0; i < 100; i++) {
+        unsigned int r= rand() % 1550;
+        unsigned int c= rand() % 1550;
+        int strIndex = rand() % 4;
+    
         if(i%2==0){
-            b1.post(r,c,ariel::Direction::Horizontal,str[strIndex]);
-        }
+            CHECK_NOTHROW(b1.post(r,c,ariel::Direction::Horizontal,str[strIndex]));
+            }
         else{
-            b1.post(r,c,ariel::Direction::Vertical,str[strIndex]);
+            CHECK_NOTHROW(b1.post(r,c,ariel::Direction::Vertical,str[strIndex]));
  
         }
+        cout<< "row: "<<r <<" col: " << c <<endl;
     }
     //b1.show();
 }
